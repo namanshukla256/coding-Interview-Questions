02528348609ff0b6349a733c14495817a02e345a
naman <namanshukla256@gmail.com>
2025-01-13 23:10:50 +0530

    13/1

---
+++ Basic JavaScript coding questions/calculate the sum of two numbers.js
@@ -0,0 +1,8 @@
+// 1. Write a JavaScript function to calculate the sum of two numbers.  
+
+//I would take two parameters and the following function can be used to calculate the sum of any 2 numbers that are passed as arguments. 
+
+
+function sumofTwoNumbers(a, b) {
+    return a +  b;
+}
